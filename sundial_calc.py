306011719362bf8b76a2dd167649bf1d248b52a6
Jon Cox <6693573+joncox123@users.noreply.github.com>
2025-01-17 02:02:40 -0700

    Update sundial_calc.py

--- sundial_calc.py
+++ sundial_calc.py
@@ -3,7 +3,7 @@
 
 MIT License
 
-Copyright (c) 2025 [Jonathan A. Cox]
+Copyright (c) 2025, Jonathan A. Cox
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
